8d74ef8e6e27397952c904af06dd0fe8554becc5
Grace Hall <graceavahall@gmail.com>
2022-07-26 22:04:13 +1000

    packaging for pypi

--- src/paths/__init__.py
+++ src/paths/__init__.py
@@ -2,14 +2,14 @@
 
 
 # files
-WRAPPER_CACHE = 'data/wrappers.json'
-CONTAINER_CACHE = 'data/container_url_cache.json'
-LOGGING_CONFIG = 'data/logging_config.yaml'
+WRAPPER_CACHE = './data/wrappers.json'
+CONTAINER_CACHE = './data/container_url_cache.json'
+LOGGING_CONFIG = './data/logging_config.yaml'
 
 # folders
-DOWNLOADED_WRAPPERS_DIR = 'data/wrappers'
-DEFAULT_TOOL_OUTDIR = 'parsed/tools'
-DEFAULT_WORKFLOW_OUTDIR = 'parsed/workflows'
+DOWNLOADED_WRAPPERS_DIR = './data/wrappers'
+DEFAULT_TOOL_OUTDIR = './parsed/tools'
+DEFAULT_WORKFLOW_OUTDIR = './parsed/workflows'
 
 
 from .managers import PathManager
